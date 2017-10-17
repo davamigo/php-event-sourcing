645ef09317042c054586b7c6d602acbdb9421da5
David Amigo <david.amigo@privalia.com>
2017-10-17 23:43:19 +0200

    Some improvements of docker containers

--- docker/phpunit.sh
+++ docker/phpunit.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+dockerfile="docker-compose.yml";
 user="david"
 service="php7.1"
 phpunit="./bin/phpunit"
+public="../public"
 
 dir=$(dirname $0)
 cd $dir
@@ -14,4 +16,18 @@ do
     shift
 done
 
-docker-compose run --rm -u $user $service php $phpunit -c tests/phpunit.xml $args
+docker-compose -f $dockerfile run --rm -u $user $service php $phpunit -c tests/phpunit.xml $args
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
+echo -e "\nCode coverage summary:"
+egrep "\s(Classes|Methods|Lines):\s(.+\%)" ../runtime/coverage/coverage.txt | head -3
+echo -e ""
+
+if [ -d "$public" ]; then
+    cd $public
+    if [ ! -e coverage ]; then
+        ln -s ../runtime/coverage/
+    fi;
+fi;
